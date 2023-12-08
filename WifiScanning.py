b47ac164b15e68d39435de23eac37fbad3562c40
khushboojayan <khushboojayan@gmail.com>
2023-12-08 10:29:50 +0000

    vendorLookup

--- WifiScanning.py
+++ WifiScanning.py
@@ -1,16 +1,22 @@
 import os
-# git push checkup
-os.system("airmon-ng start wlan0")
-os.system("airodump-ng wlan0mon")
+from mac_vendor_lookup import MacLookup
 
-BSSID = input("Enter the BSSID of the network to be sniffed: ")
+os.system("airmon-ng start wlan0")
+os.system("airodump-ng --band abg wlan0mon")
+
+BSSID = input("Enter the BSSID of any network to be sniffed: ")
 channel = input("Enter the channel this network is working on: ")
 packageName = input("Name of directory to save the package: ")
 
 os.system("mkdir DataGenerated/" + packageName)
 
-print("airodump-ng -d " + BSSID + " -c" + channel + " -w DataGenerated/"+packageName+ "/"
-          + packageName + " wlan0mon")
+os.system("sudo airodump-ng wlan0mon -c " + channel + " --bssid " + BSSID)
 
-os.system("airodump-ng -d " + BSSID + " -c" + channel + " -w DataGenerated/"+packageName+ "/"
-          + packageName + " wlan0mon")
+# "98:AF:65:B9:F3:E3" is intel
+macAddr = input("Enter the mac address for the vendor lookup: ")
+try:
+  print("\nMacAddress: " + macAddr + "\nVendor: " + MacLookup().lookup(macAddr))
+except:
+  print("Mac vendor not registered")
+
+
